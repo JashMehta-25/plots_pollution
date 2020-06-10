3236f4050dbc4aff7abaad50ac840254a0115a3c
JashMehta-25 <jashmehta999@gmail.com>
2020-06-10 16:28:37 +0530

    plot3

---
+++ plot3.R
@@ -0,0 +1,25 @@
+# Load the NEI & SCC data frames.
+
+NEI <- readRDS("G:/datasciencecoursera/datasciencecoursera/plots_pollution/exdata_data_NEI_data/summarySCC_PM25.rds")
+SCC <- readRDS("G:/datasciencecoursera/datasciencecoursera/plots_pollution/exdata_data_NEI_data/Source_Classification_Code.rds")
+
+# Subset NEI data by Baltimore's fip.
+baltimoreNEI <- NEI[NEI$fips=="24510",]
+
+# Aggregate using sum the Baltimore emissions data by year
+aggTotalsBaltimore <- aggregate(Emissions ~ year, baltimoreNEI,sum)
+
+png("plot3.png",width=480,height=480,units="px",bg="transparent")
+
+library(ggplot2)
+
+ggp <- ggplot(baltimoreNEI,aes(factor(year),Emissions,fill=type)) +
+  geom_bar(stat="identity") +
+  theme_bw() + guides(fill=FALSE)+
+  facet_grid(.~type,scales = "free",space="free") + 
+  labs(x="year", y=expression("Total PM"[2.5]*" Emission (Tons)")) + 
+  labs(title=expression("PM"[2.5]*" Emissions, Baltimore City 1999-2008 by Source Type"))
+
+print(ggp)
+
+dev.off()
